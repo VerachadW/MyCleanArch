686bf3d828f9948ff93f863ed9407f6080db0f3b
Verachad Wongsawangtham <birth24987@gmail.com>
2018-09-01 15:18:45 +0700

    Initial commit

---
+++ buildSrc/src/main/java/Dependencies.kt
@@ -0,0 +1,21 @@
+object Versions {
+    //Android
+    val androidBuildTool = "3.2.0-rc02"
+    val androidMinSDK= 16
+    val androidComplieSDK = 28
+    val androidTargetSDK = 28
+
+    val kotlin = "1.2.61"
+}
+
+object PresentationDependencies {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+}
+
+object DomainDependencies {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+}
+
+object DataDependencies {
+    val kotlin = "org.jetbrains.kotlin:kotlin-stdlib-jdk8:${Versions.kotlin}"
+}
